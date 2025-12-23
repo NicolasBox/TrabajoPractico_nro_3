e5b31a8668cca37abb80516646123d11273cce4e
Nicolas Gregorio <nicoe_97@live.com>
2025-12-23 01:59:20 -0300

    tenemos admin

--- gimnasio/admin.py
+++ gimnasio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Socio, Entrenador, Clase
 
-# Register your models here.
+admin.site.register(Socio)
+admin.site.register(Entrenador)
+admin.site.register(Clase)
